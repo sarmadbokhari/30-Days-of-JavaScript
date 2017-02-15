50651943be0eb6ddc2ae1f78d91b2d1117466b2b
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2017-02-15 11:29:22 -0800

    Sort without prefix using regex, map over list and add to DOM

--- 17 - Sort Without Articles/scripts.js
+++ 17 - Sort Without Articles/scripts.js
@@ -1,1 +1,17 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+const sortedBands = bands.sort(function(a,b) {
+  if (split(a) > split(b)) {
+    return 1;
+  } else {
+    return -1;
+  }
+});
+
+function split(phrase) {
+  return phrase.replace(/^(a |the |an )/i, '').trim();
+}
+
+document.querySelector('#bands').innerHTML = sortedBands.map(function(band) {
+  return `<li>${band}</li>`
+}).join('')
