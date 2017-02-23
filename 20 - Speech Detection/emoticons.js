8c3fdb882ffaa5a523f487929c3781121da1ec38
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2017-02-22 18:13:32 -0800

    Day 20: Speech Detection

---
+++ 20 - Speech Detection/emoticons.js
@@ -0,0 +1,975 @@
+[
+  {
+    "emoji": "😀"
+    , "description": "grinning face"
+    , "category": "People"
+    , "aliases": [
+      "grinning"
+    ]
+    , "tags": [
+      "smile"
+      , "happy"
+    ]
+    , "unicode_version": "6.1"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😃"
+    , "description": "smiling face with open mouth"
+    , "category": "People"
+    , "aliases": [
+      "smiley"
+    ]
+    , "tags": [
+      "happy"
+      , "joy"
+      , "haha"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😄"
+    , "description": "smiling face with open mouth & smiling eyes"
+    , "category": "People"
+    , "aliases": [
+      "smile"
+    ]
+    , "tags": [
+      "happy"
+      , "joy"
+      , "laugh"
+      , "pleased"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😁"
+    , "description": "grinning face with smiling eyes"
+    , "category": "People"
+    , "aliases": [
+      "grin"
+    ]
+    , "tags": [
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😆"
+    , "description": "smiling face with open mouth & closed eyes"
+    , "category": "People"
+    , "aliases": [
+      "laughing"
+      , "satisfied"
+    ]
+    , "tags": [
+      "happy"
+      , "haha"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😅"
+    , "description": "smiling face with open mouth & cold sweat"
+    , "category": "People"
+    , "aliases": [
+      "sweat_smile"
+    ]
+    , "tags": [
+      "hot"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😂"
+    , "description": "face with tears of joy"
+    , "category": "People"
+    , "aliases": [
+      "joy"
+    ]
+    , "tags": [
+      "tears"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "🤣"
+    , "description": "rolling on the floor laughing"
+    , "category": "People"
+    , "aliases": [
+      "rofl"
+    ]
+    , "tags": [
+      "lol"
+      , "laughing"
+    ]
+    , "unicode_version": "9.0"
+    , "ios_version": "10.2"
+  }
+  , {
+    "emoji": "☺️"
+    , "description": "smiling face"
+    , "category": "People"
+    , "aliases": [
+      "relaxed"
+    ]
+    , "tags": [
+      "blush"
+      , "pleased"
+    ]
+    , "unicode_version": ""
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😊"
+    , "description": "smiling face with smiling eyes"
+    , "category": "People"
+    , "aliases": [
+      "blush"
+    ]
+    , "tags": [
+      "proud"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😇"
+    , "description": "smiling face with halo"
+    , "category": "People"
+    , "aliases": [
+      "innocent"
+    ]
+    , "tags": [
+      "angel"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "🙂"
+    , "description": "slightly smiling face"
+    , "category": "People"
+    , "aliases": [
+      "slightly_smiling_face"
+    ]
+    , "tags": [
+    ]
+    , "unicode_version": "7.0"
+    , "ios_version": "9.1"
+  }
+  , {
+    "emoji": "🙃"
+    , "description": "upside-down face"
+    , "category": "People"
+    , "aliases": [
+      "upside_down_face"
+    ]
+    , "tags": [
+    ]
+    , "unicode_version": "8.0"
+    , "ios_version": "9.1"
+  }
+  , {
+    "emoji": "😉"
+    , "description": "winking face"
+    , "category": "People"
+    , "aliases": [
+      "wink"
+    ]
+    , "tags": [
+      "flirt"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😌"
+    , "description": "relieved face"
+    , "category": "People"
+    , "aliases": [
+      "relieved"
+    ]
+    , "tags": [
+      "whew"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😍"
+    , "description": "smiling face with heart-eyes"
+    , "category": "People"
+    , "aliases": [
+      "heart_eyes"
+    ]
+    , "tags": [
+      "love"
+      , "crush"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😘"
+    , "description": "face blowing a kiss"
+    , "category": "People"
+    , "aliases": [
+      "kissing_heart"
+    ]
+    , "tags": [
+      "flirt"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😗"
+    , "description": "kissing face"
+    , "category": "People"
+    , "aliases": [
+      "kissing"
+    ]
+    , "tags": [
+    ]
+    , "unicode_version": "6.1"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😙"
+    , "description": "kissing face with smiling eyes"
+    , "category": "People"
+    , "aliases": [
+      "kissing_smiling_eyes"
+    ]
+    , "tags": [
+    ]
+    , "unicode_version": "6.1"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😚"
+    , "description": "kissing face with closed eyes"
+    , "category": "People"
+    , "aliases": [
+      "kissing_closed_eyes"
+    ]
+    , "tags": [
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😋"
+    , "description": "face savouring delicious food"
+    , "category": "People"
+    , "aliases": [
+      "yum"
+    ]
+    , "tags": [
+      "tongue"
+      , "lick"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😜"
+    , "description": "face with stuck-out tongue & winking eye"
+    , "category": "People"
+    , "aliases": [
+      "stuck_out_tongue_winking_eye"
+    ]
+    , "tags": [
+      "prank"
+      , "silly"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😝"
+    , "description": "face with stuck-out tongue & closed eyes"
+    , "category": "People"
+    , "aliases": [
+      "stuck_out_tongue_closed_eyes"
+    ]
+    , "tags": [
+      "prank"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😛"
+    , "description": "face with stuck-out tongue"
+    , "category": "People"
+    , "aliases": [
+      "stuck_out_tongue"
+    ]
+    , "tags": [
+    ]
+    , "unicode_version": "6.1"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "🤑"
+    , "description": "money-mouth face"
+    , "category": "People"
+    , "aliases": [
+      "money_mouth_face"
+    ]
+    , "tags": [
+      "rich"
+    ]
+    , "unicode_version": "8.0"
+    , "ios_version": "9.1"
+  }
+  , {
+    "emoji": "🤗"
+    , "description": "hugging face"
+    , "category": "People"
+    , "aliases": [
+      "hugs"
+    ]
+    , "tags": [
+    ]
+    , "unicode_version": "8.0"
+    , "ios_version": "9.1"
+  }
+  , {
+    "emoji": "🤓"
+    , "description": "nerd face"
+    , "category": "People"
+    , "aliases": [
+      "nerd_face"
+    ]
+    , "tags": [
+      "geek"
+      , "glasses"
+    ]
+    , "unicode_version": "8.0"
+    , "ios_version": "9.1"
+  }
+  , {
+    "emoji": "😎"
+    , "description": "smiling face with sunglasses"
+    , "category": "People"
+    , "aliases": [
+      "sunglasses"
+    ]
+    , "tags": [
+      "cool"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "🤡"
+    , "description": "clown face"
+    , "category": "People"
+    , "aliases": [
+      "clown_face"
+    ]
+    , "tags": [
+    ]
+    , "unicode_version": "9.0"
+    , "ios_version": "10.2"
+  }
+  , {
+    "emoji": "🤠"
+    , "description": "cowboy hat face"
+    , "category": "People"
+    , "aliases": [
+      "cowboy_hat_face"
+    ]
+    , "tags": [
+    ]
+    , "unicode_version": "9.0"
+    , "ios_version": "10.2"
+  }
+  , {
+    "emoji": "😏"
+    , "description": "smirking face"
+    , "category": "People"
+    , "aliases": [
+      "smirk"
+    ]
+    , "tags": [
+      "smug"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😒"
+    , "description": "unamused face"
+    , "category": "People"
+    , "aliases": [
+      "unamused"
+    ]
+    , "tags": [
+      "meh"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😞"
+    , "description": "disappointed face"
+    , "category": "People"
+    , "aliases": [
+      "disappointed"
+    ]
+    , "tags": [
+      "sad"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😔"
+    , "description": "pensive face"
+    , "category": "People"
+    , "aliases": [
+      "pensive"
+    ]
+    , "tags": [
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😟"
+    , "description": "worried face"
+    , "category": "People"
+    , "aliases": [
+      "worried"
+    ]
+    , "tags": [
+      "nervous"
+    ]
+    , "unicode_version": "6.1"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😕"
+    , "description": "confused face"
+    , "category": "People"
+    , "aliases": [
+      "confused"
+    ]
+    , "tags": [
+    ]
+    , "unicode_version": "6.1"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "🙁"
+    , "description": "slightly frowning face"
+    , "category": "People"
+    , "aliases": [
+      "slightly_frowning_face"
+    ]
+    , "tags": [
+    ]
+    , "unicode_version": "7.0"
+    , "ios_version": "9.1"
+  }
+  , {
+    "emoji": "☹️"
+    , "description": "frowning face"
+    , "category": "People"
+    , "aliases": [
+      "frowning_face"
+    ]
+    , "tags": [
+    ]
+    , "unicode_version": ""
+    , "ios_version": "9.1"
+  }
+  , {
+    "emoji": "😣"
+    , "description": "persevering face"
+    , "category": "People"
+    , "aliases": [
+      "persevere"
+    ]
+    , "tags": [
+      "struggling"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😖"
+    , "description": "confounded face"
+    , "category": "People"
+    , "aliases": [
+      "confounded"
+    ]
+    , "tags": [
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😫"
+    , "description": "tired face"
+    , "category": "People"
+    , "aliases": [
+      "tired_face"
+    ]
+    , "tags": [
+      "upset"
+      , "whine"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😩"
+    , "description": "weary face"
+    , "category": "People"
+    , "aliases": [
+      "weary"
+    ]
+    , "tags": [
+      "tired"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😤"
+    , "description": "face with steam from nose"
+    , "category": "People"
+    , "aliases": [
+      "triumph"
+    ]
+    , "tags": [
+      "smug"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😠"
+    , "description": "angry face"
+    , "category": "People"
+    , "aliases": [
+      "angry"
+    ]
+    , "tags": [
+      "mad"
+      , "annoyed"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😡"
+    , "description": "pouting face"
+    , "category": "People"
+    , "aliases": [
+      "rage"
+      , "pout"
+    ]
+    , "tags": [
+      "angry"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😶"
+    , "description": "face without mouth"
+    , "category": "People"
+    , "aliases": [
+      "no_mouth"
+    ]
+    , "tags": [
+      "mute"
+      , "silence"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😐"
+    , "description": "neutral face"
+    , "category": "People"
+    , "aliases": [
+      "neutral_face"
+    ]
+    , "tags": [
+      "meh"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😑"
+    , "description": "expressionless face"
+    , "category": "People"
+    , "aliases": [
+      "expressionless"
+    ]
+    , "tags": [
+    ]
+    , "unicode_version": "6.1"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😯"
+    , "description": "hushed face"
+    , "category": "People"
+    , "aliases": [
+      "hushed"
+    ]
+    , "tags": [
+      "silence"
+      , "speechless"
+    ]
+    , "unicode_version": "6.1"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😦"
+    , "description": "frowning face with open mouth"
+    , "category": "People"
+    , "aliases": [
+      "frowning"
+    ]
+    , "tags": [
+    ]
+    , "unicode_version": "6.1"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😧"
+    , "description": "anguished face"
+    , "category": "People"
+    , "aliases": [
+      "anguished"
+    ]
+    , "tags": [
+      "stunned"
+    ]
+    , "unicode_version": "6.1"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😮"
+    , "description": "face with open mouth"
+    , "category": "People"
+    , "aliases": [
+      "open_mouth"
+    ]
+    , "tags": [
+      "surprise"
+      , "impressed"
+      , "wow"
+    ]
+    , "unicode_version": "6.1"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😲"
+    , "description": "astonished face"
+    , "category": "People"
+    , "aliases": [
+      "astonished"
+    ]
+    , "tags": [
+      "amazed"
+      , "gasp"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😵"
+    , "description": "dizzy face"
+    , "category": "People"
+    , "aliases": [
+      "dizzy_face"
+    ]
+    , "tags": [
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😳"
+    , "description": "flushed face"
+    , "category": "People"
+    , "aliases": [
+      "flushed"
+    ]
+    , "tags": [
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😱"
+    , "description": "face screaming in fear"
+    , "category": "People"
+    , "aliases": [
+      "scream"
+    ]
+    , "tags": [
+      "horror"
+      , "shocked"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😨"
+    , "description": "fearful face"
+    , "category": "People"
+    , "aliases": [
+      "fearful"
+    ]
+    , "tags": [
+      "scared"
+      , "shocked"
+      , "oops"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😰"
+    , "description": "face with open mouth & cold sweat"
+    , "category": "People"
+    , "aliases": [
+      "cold_sweat"
+    ]
+    , "tags": [
+      "nervous"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😢"
+    , "description": "crying face"
+    , "category": "People"
+    , "aliases": [
+      "cry"
+    ]
+    , "tags": [
+      "sad"
+      , "tear"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😥"
+    , "description": "disappointed but relieved face"
+    , "category": "People"
+    , "aliases": [
+      "disappointed_relieved"
+    ]
+    , "tags": [
+      "phew"
+      , "sweat"
+      , "nervous"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "🤤"
+    , "description": "drooling face"
+    , "category": "People"
+    , "aliases": [
+      "drooling_face"
+    ]
+    , "tags": [
+    ]
+    , "unicode_version": "9.0"
+    , "ios_version": "10.2"
+  }
+  , {
+    "emoji": "😭"
+    , "description": "loudly crying face"
+    , "category": "People"
+    , "aliases": [
+      "sob"
+    ]
+    , "tags": [
+      "sad"
+      , "cry"
+      , "bawling"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😓"
+    , "description": "face with cold sweat"
+    , "category": "People"
+    , "aliases": [
+      "sweat"
+    ]
+    , "tags": [
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😪"
+    , "description": "sleepy face"
+    , "category": "People"
+    , "aliases": [
+      "sleepy"
+    ]
+    , "tags": [
+      "tired"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "😴"
+    , "description": "sleeping face"
+    , "category": "People"
+    , "aliases": [
+      "sleeping"
+    ]
+    , "tags": [
+      "zzz"
+    ]
+    , "unicode_version": "6.1"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "🙄"
+    , "description": "face with rolling eyes"
+    , "category": "People"
+    , "aliases": [
+      "roll_eyes"
+    ]
+    , "tags": [
+    ]
+    , "unicode_version": "8.0"
+    , "ios_version": "9.1"
+  }
+  , {
+    "emoji": "🤔"
+    , "description": "thinking face"
+    , "category": "People"
+    , "aliases": [
+      "thinking"
+    ]
+    , "tags": [
+    ]
+    , "unicode_version": "8.0"
+    , "ios_version": "9.1"
+  }
+  , {
+    "emoji": "🤥"
+    , "description": "lying face"
+    , "category": "People"
+    , "aliases": [
+      "lying_face"
+    ]
+    , "tags": [
+      "liar"
+    ]
+    , "unicode_version": "9.0"
+    , "ios_version": "10.2"
+  }
+  , {
+    "emoji": "😬"
+    , "description": "grimacing face"
+    , "category": "People"
+    , "aliases": [
+      "grimacing"
+    ]
+    , "tags": [
+    ]
+    , "unicode_version": "6.1"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "🤐"
+    , "description": "zipper-mouth face"
+    , "category": "People"
+    , "aliases": [
+      "zipper_mouth_face"
+    ]
+    , "tags": [
+      "silence"
+      , "hush"
+    ]
+    , "unicode_version": "8.0"
+    , "ios_version": "9.1"
+  }
+  , {
+    "emoji": "🤢"
+    , "description": "nauseated face"
+    , "category": "People"
+    , "aliases": [
+      "nauseated_face"
+    ]
+    , "tags": [
+      "sick"
+      , "barf"
+      , "disgusted"
+    ]
+    , "unicode_version": "9.0"
+    , "ios_version": "10.2"
+  }
+  , {
+    "emoji": "🤧"
+    , "description": "sneezing face"
+    , "category": "People"
+    , "aliases": [
+      "sneezing_face"
+    ]
+    , "tags": [
+      "achoo"
+      , "sick"
+    ]
+    , "unicode_version": "9.0"
+    , "ios_version": "10.2"
+  }
+  , {
+    "emoji": "😷"
+    , "description": "face with medical mask"
+    , "category": "People"
+    , "aliases": [
+      "mask"
+    ]
+    , "tags": [
+      "sick"
+      , "ill"
+    ]
+    , "unicode_version": "6.0"
+    , "ios_version": "6.0"
+  }
+  , {
+    "emoji": "🤒"
+    , "description": "face with thermometer"
+    , "category": "People"
+    , "aliases": [
+      "face_with_thermometer"
+    ]
+    , "tags": [
+      "sick"
+    ]
+    , "unicode_version": "8.0"
+    , "ios_version": "9.1"
+  }
+]
