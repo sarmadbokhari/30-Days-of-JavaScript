2fb3dfca36b49c41b2df1d69ffbb428bf601e4ca
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2017-02-27 11:49:46 -0800

    Fix with scroll calculation

--- 22 - Follow Along Link Highlighter/scripts.js
+++ 22 - Follow Along Link Highlighter/scripts.js
@@ -6,9 +6,18 @@ document.body.append(highlight);
 
 function hightlightLink() {
   const linkCoords = this.getBoundingClientRect();
+
+  // Add the amount scrolled on page to the top (scrollY) and left (scrollX for horizontal scroll)
+  const coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY,
+    left: linkCoords.left + window.scrollX
+  }
+
   highlight.style.width = linkCoords.width + 'px';
   highlight.style.height = linkCoords.height + 'px';
-  highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`
+  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`
 }
 
 triggers.forEach(a => a.addEventListener('mouseenter', hightlightLink));
