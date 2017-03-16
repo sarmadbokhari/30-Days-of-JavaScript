52c635f81a56cd79be800db8be33a22065bf024a
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2017-03-16 12:11:00 -0700

    Update implementation of countdown timer

--- 29 - Countdown Timer/scripts.js
+++ 29 - Countdown Timer/scripts.js
@@ -1,9 +1,16 @@
 let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]')
 
 function timer(seconds) {
+  // clear any existing timers
+  clearInterval(countdown);
+
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds); // run immediately to show first time instance
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -18,8 +25,34 @@ function timer(seconds) {
 }
 
 function displayTimeLeft(seconds) {
-  const minutes = Math.floor(seconds/60);
+  const minutes = Math.floor(seconds/60); // get whole minutes
   const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+
+  timerDisplay.textContent = display;
+  document.title = display;
 
   console.log({ minutes, remainderSeconds });
 }
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minute = end.getMinutes();
+
+  endTime.textContent = `Be back at ${hour > 12 ? hour - 12 : hour}:${minute < 10 ? '0' : ''}${minute}`;
+}
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+
+  timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const mins = this.minutes.value;
+  timer(mins*60);
+  this.reset();
+});
